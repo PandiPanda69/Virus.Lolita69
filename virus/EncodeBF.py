0a4540421cb3e6a4d3acd0e5ac4e5c7dd1347e0e
vbailly <vincentbaill@gmail.com>
2013-02-19 20:35:20 +0100

    Update virus/EncodeBF.py
    Obfuscation!

--- virus/EncodeBF.py
+++ virus/EncodeBF.py
@@ -1,23 +1,10 @@
 import sys
 import random
 random.seed
-k = "".join([chr(i) for i in random.sample(range(256),5)])
-T = range(10,260,20)
-p = 0
-x = ord
+
+L,C,k,T,p,x,kde,br=["k[1]*(x(c)/20-p)","k[0]*(p-x(c)/20)","k[2]*(x(c)-T[x(c)/20])","k[3]*(T[x(c)/20]-x(c))"] ,["p < x(c)/20","p > x(c)/20","T[x(c)/20] < x(c)","T[x(c)/20] > x(c)"] ,"".join([chr(i) for i in random.sample(range(256),5)]), range(10,260,20),0,ord ,compile("""k += "".join([br(L[i]) for i in range(4) if br(C[i])])+k[4]; T[x(c)/20] = x(c); p = x(c)/20""",'a','exec'),eval
+
 for c in sys.stdin.read():
-	if p < x(c)/20:
-		k += k[1]*(x(c)/20-p)
-	if p > x(c)/20:
-		k += k[0]*(p-x(c)/20)
-	p = x(c)/20
-	if T[x(c)/20] < x(c):
-		k += k[2]*(x(c)-T[x(c)/20])
-	if T[x(c)/20] > x(c):
-		k += k[3]*(T[x(c)/20]-x(c))
-	T[x(c)/20] = x(c)
-	k += k[4]
+	br(kde)
 print k
-
-
 

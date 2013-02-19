988c5b90af33ae876f3ec9c5e4cd859cfb798766
vbailly <vincentbaill@gmail.com>
2013-02-19 18:53:43 +0100

    Update virus/DecodeBF.py
    opcode coded by a key

--- virus/DecodeBF.py
+++ virus/DecodeBF.py
@@ -3,15 +3,18 @@ import sys
 table = range(10,260,20)
 ptr = 0
 code = sys.stdin.read()
+key = code[:5]
+code = code[5:]
+
 for opcode in code:
-  if opcode == "<":
+	if opcode == key[0]:
 		ptr -= 1
-	if opcode == ">":
+	if opcode == key[1]:
 		ptr += 1
-	if opcode == "+":
+	if opcode == key[2]:
 		table[ptr] = table[ptr] + 1
-	if opcode == "-":
+	if opcode == key[3]:
 		table[ptr] = table[ptr] - 1
-	if opcode == ".":
+	if opcode == key[4]:
 		sys.stdout.write("%c"%table[ptr])
 		

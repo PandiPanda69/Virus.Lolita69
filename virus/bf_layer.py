73827485d1df7b33adfe165d27a3f34f7c90c50a
vbailly <vincentbaill@gmail.com>
2013-02-20 13:35:10 +0100

    Update virus/bf_layer.py

--- virus/bf_layer.py
+++ virus/bf_layer.py
@@ -4,7 +4,7 @@ import sys
 
 
 L,C,k,T,p,x,kde,br=["k[1]*(x(c)/20-p)","k[0]*(p-x(c)/20)","k[2]*(x(c)-T[x(c)/20])","k[3]*(T[x(c)/20]-x(c))"] ,["p < x(c)/20","p > x(c)/20","T[x(c)/20] < x(c)","T[x(c)/20] > x(c)"] ,"".join([chr(i) for i in random.sample(range(256),5)]), range(10,260,20),0,ord ,compile("""k += "".join([br(L[i]) for i in range(4) if br(C[i])])+k[4]; T[x(c)/20] = x(c); p = x(c)/20""",'a','exec'),eval
-loc = locals()
+loc = dict(locals())
 def crypt(clear):
   for o in clear:
 		loc['c'] = o
@@ -12,7 +12,7 @@ def crypt(clear):
 	return k
 
 DECRYPT = """
-loc = locals()
+loc = dict(locals())
 w,t,p,h ="", range(10,260,20),0,["p-=1","p+=1","t[p]+=1","t[p]-=1","w+='%c'%t[p]"]
 k=[compile(x,'a','exec') for x in h]
 def decrypt(crypted):

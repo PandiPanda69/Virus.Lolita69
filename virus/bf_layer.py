df569a69b43a0c874e7e638900d527e8c27fa5c7
vbailly <vincentbaill@gmail.com>
2013-02-20 04:10:47 -0800

    Create bf_layer.py

---
+++ virus/bf_layer.py
@@ -0,0 +1,23 @@
+import random
+random.seed
+import sys
+
+
+L,C,k,T,p,x,kde,br=["k[1]*(x(c)/20-p)","k[0]*(p-x(c)/20)","k[2]*(x(c)-T[x(c)/20])","k[3]*(T[x(c)/20]-x(c))"] ,["p < x(c)/20","p > x(c)/20","T[x(c)/20] < x(c)","T[x(c)/20] > x(c)"] ,"".join([chr(i) for i in random.sample(range(256),5)]), range(10,260,20),0,ord ,compile("""k += "".join([br(L[i]) for i in range(4) if br(C[i])])+k[4]; T[x(c)/20] = x(c); p = x(c)/20""",'a','exec'),eval
+loc = locals()
+def crypt(clear):
+  for o in clear:
+		loc['c'] = o
+		br(kde,globals(),loc)
+	return k
+
+DECRYPT = """
+loc = locals()
+w,t,p,h ="", range(10,260,20),0,["p-=1","p+=1","t[p]+=1","t[p]-=1","w+='%c'%t[p]"]
+k=[compile(x,'a','exec') for x in h]
+def decrypt(crypted):
+	loc["c"] = crypted
+	[r for r in[eval(k[i],globals(),loc)  for o in c[5:] for i in range(5)if o==c[i]] for r in c[5:]]	
+	return w
+"""
+

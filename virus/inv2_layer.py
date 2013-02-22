661e3a04bffae738fa1602f55313826253a3731b
ClementNotin <clement@notin.org>
2013-02-22 20:59:14 +0100

    Formatage des layers invAll et inv2

--- virus/inv2_layer.py
+++ virus/inv2_layer.py
@@ -1,17 +1,18 @@
 def crypt(clear):
-  n = []
-	for i in range(len(clear)/2):
-		n += [clear[2*i+1],clear[2*i]]
-	if len(clear) % 2 == 1:
-		n+= [clear[-1]]
-	return "".join(n)
+    n = []
+    for i in range(len(clear) / 2):
+        n += [clear[2 * i + 1], clear[2 * i]]
+    if len(clear) % 2 == 1:
+        n += [clear[-1]]
+    return "".join(n)
+
 
 DECRYPT = """
 def decrypt(crypted):
-	n = []
-	for i in range(len(crypted)/2):
-		n += [crypted[2*i+1],crypted[2*i]]
-	if len(crypted) % 2 == 1:
-		n+= [crypted[-1]]
-	return "".join(n)
+    n = []
+    for i in range(len(crypted) / 2):
+        n += [crypted[2 * i + 1], crypted[2 * i]]
+    if len(crypted) % 2 == 1:
+        n += [crypted[-1]]
+    return "".join(n)
 """
--- virus/invAll_layer.py
+++ virus/invAll_layer.py
@@ -1,12 +1,14 @@
 def crypt(clear):
-        n = []
-        for i in range(len(clear)):
-                n += clear[-i-1]
-        return "".join(n)
+    n = []
+    for i in range(len(clear)):
+        n += clear[-i - 1]
+    return "".join(n)
+
 
 DECRYPT = """
 def decrypt(crypted):
-        n = []
-        for i in range(len(crypted)):
-                n += crypted[-i-1]
-        return "".join(n)
+    n = []
+    for i in range(len(crypted)):
+        n += crypted[-i - 1]
+    return "".join(n)
+"""

004daa1d640693440a3d248a63d6a93e1820dd49
ClementNotin <clement@notin.org>
2013-02-22 20:59:49 +0100

    Suppression fichiers inutilisés

--- virus/DecodeBF.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-t,p,c,h = range(10,260,20),0,sys.stdin.read(),["p-=1","p+=1","t[p]+=1","t[p]-=1","sys.stdout.write('%c'%t[p])"]
-k=[compile(x,'a','exec') for x in h]
-[r for r in[eval(k[i])  for o in c[5:] for i in range(5)if o==c[i]] for r in c[5:]]
-		

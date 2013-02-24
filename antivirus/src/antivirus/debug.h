67523e89c211c53e508b370c2ca0fa9c908b171d
PandiPanda69 <sebastien.meriot@insa-lyon.fr>
2013-02-24 19:43:37 +0100

    [Antivirus] Code simplification

--- antivirus/src/antivirus/debug.h
+++ antivirus/src/antivirus/debug.h
@@ -5,16 +5,8 @@
 
 
 #ifdef DEBUG
-	#define TRACE(__str)   debug_trace(__str, __FILE__, __LINE__)
-
 	#include <iostream>
-	
-	using namespace std;
-
-	void debug_trace(const char* str, const char* file, int line)
-	{
-		cout << "[TRACE] " << file << ":" << line << "\t" << str << endl;
-	}
+	#define TRACE(__str)  std::cout << "[TRACE] " << __FILE__ << ":" << __LINE__ << "\t" << __str << std::endl;
 #else
 	#define TRACE(__str)	; 
 #endif

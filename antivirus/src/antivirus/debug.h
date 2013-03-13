490e50fe9ea499cf4257ca8bd396890f532fd4ab
PandiPanda69 <sebastien.meriot@insa-lyon.fr>
2013-03-13 19:07:41 +0100

    [Antivirus] Remove PID display. It was a bad idea.

--- antivirus/src/antivirus/debug.h
+++ antivirus/src/antivirus/debug.h
@@ -6,8 +6,7 @@
 
 #ifdef DEBUG
 	#include <iostream>
-	#include <unistd.h>
-	#define TRACE(__str)  std::cout << "[TRACE] " << __FILE__ << ":" << __LINE__ << " pid:" << getpid() << "\t" << __str << std::endl;
+	#define TRACE(__str)  std::cout << "[TRACE] " << __FILE__ << ":" << __LINE__ << "\t" << __str << std::endl;
 #else
 	#define TRACE(__str)	; 
 #endif
